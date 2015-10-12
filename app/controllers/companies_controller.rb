0bf62456c9b8d2f718329bb639a44f8734e8c3cc
Spencer Rohan <spencermrohan@gmail.com>
2015-10-12 00:43:52 -0500

    add top 20 search results

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,17 +1,14 @@
 class CompaniesController < ApplicationController
 
-  def form
-
-  end
-
   def search
    symbols_query = Company.where("LOWER(symbol) LIKE ?", "%#{params[:query].downcase}%")
    names_query = Company.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%")
-   @results = (symbols_query + names_query).uniq
+   results = (symbols_query + names_query).uniq
+   @results = results.sort { |a, b| a.name <=> b.name }.first(20)
     render :"companies/index"
   end
 
-  def index #show results
+  def index
 
   end
 
