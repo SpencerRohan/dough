20828f71b33cbeda1f1a8a62ccf4ed1471bb970c
Spencer Rohan <spencermrohan@gmail.com>
2015-10-12 03:14:04 -0500

    graph and data projection complete

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -18,7 +18,17 @@ class CompaniesController < ApplicationController
     data = yahoo_client.historical_quotes(@company.symbol, { start_date: Time::now-(24*60*60*30), end_date: Time::now })
     @daily_average = data.map do |day|
       [day.low.to_f, day.high.to_f, day.open.to_f, day.close.to_f].reduce(:+)/4.0
-    end
+    end.reverse
+    range = [0, @daily_average.max, @daily_average.max/100]
+    @graph = Gchart.line( title: @company.name,
+                          data: @daily_average,
+                          min_value: @daily_average.min,
+                          max_value: @daily_average.max,
+                          axis_with_labels: ['x', 'y'],
+                          axis_range: [[1,11,21,30], range],
+                          axis_labels: [[data[-1].trade_date, data[data.count/2].trade_date, 'Today']])
+
+
   end
 
 end
